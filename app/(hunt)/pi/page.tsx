23627d8d1064aac73041285806d6fd22e7ae2a21
Varun Shenoy <varun123shenoy@gmail.com>
2025-03-14 12:28:39 +0530

    fix: passes linter now

--- app/(hunt)/pi/page.tsx
+++ app/(hunt)/pi/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
     return (
         <div className="text-white h-full content-center text-center w-screen flex-grow">
-            Oh No! Looks like you were too late! Bet you wish you could visit the past.
+            Oh No! Looks like you were too late! Bet you wish you could visit
+            the past.
         </div>
     )
 }
